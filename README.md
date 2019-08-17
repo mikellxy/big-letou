4bf29657ccf2359332cdc35fecbb1d38e9990768
李心宇 <lixinyu@lixinyudeMBP.lan>
2019-08-18 00:23:22 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # big-letou
-对大乐透彩票
+对大乐透彩票  
+
+requests模块获取大乐透开奖网站的html文本：  
+```.env
+url = 'http://kaijiang.500.com/shtml/dlt/19095.shtml'
+resp = requests.get(url)
+html_text = resp.content.decode('GBK')
+```
+注意在浏览器的console里获取网站的charset，自行对resp.content解码，有些网站没有在html head里返回
+编码，resp.text的结果里可能会有乱码：  
+```.env
+> document.characterSet
+< "GBK"
+```  
+观察html的构成，前区的5个号码在class="ball_red"的li标签里，
+后区的2个号码在class="ball_blue"的li标签里，这里通过正则分别获取前区
+和后区号码的列表即可：  
+```.env
+reg = re.compile(r'<li class="ball_blue">(\d+)</li>')
+back_balls = reg.findall(html_text)
+result_nums = ' '.join(front_balls) + ' + ' + ' '.join(back_balls)
+print('本期开奖号码为：\033[0;31m%s\033[0m' % result_nums)
+```
+最后让用户输入一注号码，跟开奖结果进行比较，输出结果：  
+```.env
+def result_handler(results, num, is_matched):
+    if is_matched:
+        res = '\033[0;31m%s\033[0m' % num
+    else:
+        res = '\033[0;32m%s\033[0m' % num
+    results.append(res)
+```
---
+++ big_letou.py
@@ -0,0 +1,44 @@
+import requests
+import re
+
+url = 'http://kaijiang.500.com/shtml/dlt/19095.shtml'
+resp = requests.get(url)
+html_text = resp.content.decode('GBK')
+
+reg = re.compile(r'<span class="span_right">(.+)</span>')
+dt = reg.findall(html_text)[0][5:15]
+print('开奖日期为：', dt)
+
+reg = re.compile(r'<li class="ball_red">(\d+)</li>')
+front_balls = reg.findall(html_text)
+
+reg = re.compile(r'<li class="ball_blue">(\d+)</li>')
+back_balls = reg.findall(html_text)
+result_nums = ' '.join(front_balls) + ' + ' + ' '.join(back_balls)
+print('本期开奖号码为：\033[0;31m%s\033[0m' % result_nums)
+
+nums = input("请输入一注号码，号码之间以空格分割\n")
+nums = nums.split(' ')
+
+front_nums = nums[:5]
+back_nums = nums[5:]
+
+
+def result_handler(results, num, is_matched):
+    if is_matched:
+        res = '\033[0;31m%s\033[0m' % num
+    else:
+        res = '\033[0;32m%s\033[0m' % num
+    results.append(res)
+
+
+results = []
+for num in front_nums:
+    is_matched = True if num in front_balls else False
+    result_handler(results, num, is_matched)
+for num in back_nums:
+    is_matched = True if num in back_balls else False
+    result_handler(results, num, is_matched)
+
+print('\033[0;31m红色\033[0m为中奖号码，\033[0;32m绿色\033[0m为非中奖号码，你这注号码的结果为...:')
+print(' '.join(results[0:5]) + ' + ' + ' '.join(results[5:]))
